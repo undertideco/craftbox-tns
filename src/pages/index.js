17267a5532157d720938a7ada5a9c7c3336b899c
Duncan Leo <duncanleo97@gmail.com>
2017-07-10 17:10:00 +0800

    basic index page

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default () => 
+  <div style={{ color: `pink` }}>
+    <h1>Hello Gatsby!</h1>
+    <p>What a world!</p>
+    <img src="http://lorempixel.com/400/200/" />
+  </div>
