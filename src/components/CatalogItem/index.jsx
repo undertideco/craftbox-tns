a2758aa951d75b58c665d7be7643118f6bc2b768
Duncan Leo <duncanleo97@gmail.com>
2017-07-11 15:21:18 +0800

    remove item materials

--- src/components/CatalogItem/index.jsx
+++ src/components/CatalogItem/index.jsx
@@ -10,17 +10,6 @@ class CatalogItem extends Component {
         <div className="item-details">
           <span className="item-title">{item.title}</span>
           <span className="item-price">S${item.price}</span>
-          <ul className="item-materials">
-            <span>Materials:</span>
-            {
-              item.materials.map(material => (
-                <li>
-                  <span className="material-title">{material.title}</span>
-                  <span className="material-quantity">{material.quantity}</span>
-                </li>
-              ))
-            }
-          </ul>
         </div>
       </li>
     );
