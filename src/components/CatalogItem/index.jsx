99e22a1c9e16654ad38b325ff133a86d72c4df7a
Duncan Leo <duncanleo97@gmail.com>
2017-07-12 15:17:31 +0800

    add display of fake database items

--- src/components/CatalogItem/index.jsx
+++ src/components/CatalogItem/index.jsx
@@ -1,17 +1,20 @@
 import React, { Component, PropTypes } from 'react';
+import Link from 'gatsby-link';
 import itemStyles from './item.css';
 
 class CatalogItem extends Component {
   render() {
     const { item } = this.props;
     return (
-      <li className="item" styles={itemStyles}>
-        <img src={item.image} alt="" className="item-image" />
-        <div className="item-details">
-          <span className="item-title">{item.title}</span>
-          <span className="item-price">S${item.price}</span>
-        </div>
-      </li>
+      <Link to={`/product?id=${item.id}`}>
+        <li className="item" styles={itemStyles}>
+          <img src={item.image} alt="" className="item-image" />
+          <div className="item-details">
+            <span className="item-title">{item.title}</span>
+            <span className="item-price">S${item.price}</span>
+          </div>
+        </li>
+      </Link>
     );
   }
 }
--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Database from '../database';
 
-export default () => (
-  <div style={{ color: 'pink' }}>
-    <h1>PRODUCT PAGE!</h1>
-    <p>What a world!</p>
-    <img src="http://lorempixel.com/400/200/" alt="" />
-  </div>
-);
+export default class Product extends Component {
+  render() {
+    const productID = this.props.location.search.match(/id=(\d)/)[1];
+    const product = Database.getById(parseInt(productID));
+    return (
+      <div>
+        <h1>{product.title}</h1>
+        <h4 className="price">${product.price}</h4>
+        <p>{product.description}</p>
+        <img src={product.image} alt="" />
+
+        <ul className="materials">
+          {
+            product.materials.map(material => (
+              <li className="material">
+                <span className="title">{material.title}</span>
+                <span className="qty">{material.quantity}</span>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    );
+  }
+}
 
