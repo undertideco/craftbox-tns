97ef19b8262985e1d5ca1a055aaaf5ff0e25379a
Jurvis Tan <jurvis.tan@me.com>
2017-07-13 11:46:48 +0800

    remove unneeded layout HTML elements

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,35 +1,12 @@
 import React from 'react';
-import Navigation from '../components/Navigation';
-import Search from '../components/Search';
-import indexStyle from './index.css';
+import Navigation from '../components/Navigation/navigation';
 
 export default class Template extends React.Component {
   render() {
     return (
-      <div className="root-container" styles={indexStyle}>
-        <div className="header">
-          <img className="icon" src="http://via.placeholder.com/40x40" alt=""/>
-          <div className="title">
-            <span
-              style={{
-                fontSize: '2em',
-                fontWeight: '600',
-                color: 'white',
-              }}
-            >CraftBox</span>
-          </div>
-          <div
-            style={{
-              flexGrow: '1',
-            }}
-          >
-            <Navigation />
-          </div>
-          <Search />
-        </div>
-        <div className="content">
-          {this.props.children()}
-        </div>
+      <div className="root-container">
+        <Navigation />
+        {this.props.children()}
       </div>
     );
   }
