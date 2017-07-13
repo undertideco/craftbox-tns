9076755e425a8a81e53a5c87c3d3b6aeb8ea5a90
Jurvis Tan <jurvis.tan@me.com>
2017-07-13 19:45:19 +0800

    add padding between catalog items

--- src/components/CatalogItem/catalog_item.jsx
+++ src/components/CatalogItem/catalog_item.jsx
@@ -5,12 +5,12 @@ class CatalogItem extends Component {
   render() {
     const { item } = this.props;
     return (
-      <article className="fl w-100 w-25-m w-20-l mv4 br2 ba dark-gray b--black-10">
+      <article className="fl w-100 w-25-m w-20-l mv4 pa2-ns">
         <Link className="db link dim" to={`/product?id=${item.id}`}>
-          <img src={item.image} className="db w-100 br2 br--top" alt="Photo of a kitten looking menacing." />
+          <img src={item.image} className="db w-100" alt="Photo of a kitten looking menacing." />
           <dl className="mt2 f6 lh-copy">
             <dt className="clip">Title</dt>
-            <dd className="f5 f4-ns ml0 black truncate w-100">{item.title}</dd>
+            <dd className="f5 fw5 ml0 black truncate w-100">{item.title}</dd>
             <dt className="clip">Artist</dt>
             <dd className="f5 ml0 gray truncate w-100">${item.price}</dd>
           </dl>
