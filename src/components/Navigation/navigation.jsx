bc0bf8353440940ede08209a52fa9ad54ed45ba7
Jurvis Tan <jurvis.tan@me.com>
2017-07-13 20:12:54 +0800

    make navigation bar responsive

--- src/components/Navigation/navigation.jsx
+++ src/components/Navigation/navigation.jsx
@@ -5,12 +5,12 @@ import Search from '../Search/search';
 export default class Navigation extends Component {
   render() {
     return (
-      <div className="dt w-100 pa3 mw8 bg-blue h3">
-        <h1 className="dtc f3 w-20 white lh-title v-mid">CraftBox</h1>
+      <div className="dt w-100 pa3 bg-blue h3">
+        <h1 className="dtc-ns dt-row f3 w-20 white lh-title v-mid-l">CraftBox</h1>
         <Search />
-        <div className="dtc v-mid tr pa3">
-          <Link className="f6 fw4 hover-white no-underline white-70 dn dib-ns pv2 ph3" to="/">Catalog</Link>
-          <Link className="f6 fw4 hover-white no-underline white-70 dn dib-ns pv2 ph3" to="/product">Somewhere Else</Link>
+        <div className="dtc-ns dt-row v-mid-l tl tr-ns">
+          <Link className="f6 fw4 hover-white no-underline white-70 pr2" to="/">Catalog</Link>
+          <Link className="f6 fw4 hover-white no-underline white-70 pr2" to="/product">Inspiration</Link>
         </div>
       </div>
     );
