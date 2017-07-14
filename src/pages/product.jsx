92464da78769a2dde220d836852cbf8b582912b9
Jurvis Tan <jurvis.tan@me.com>
2017-07-14 22:14:10 +0800

    scaffold product page

---
+++ src/pages/product.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import Database from '../database';
+import YouTube from '../components/YouTube/youtube';
+
+export default class Product extends Component {
+  render() {
+    const productID = this.props.location.search.match(/id=(\d)/)[1];
+    const product = Database.getById(parseInt(productID));
+    return (
+      <div className="dt pa3 mw9">
+        <section className="dt-row fl db">
+          <div className="fl w-100 w-40-ns">
+            <img src={product.image} alt="" />
+          </div>
+          <div className="fl w-100 w-60-ns ph4-ns">
+            <h1 className="f3 ln-title">{product.title}</h1>
+            <p className="f5 lh-copy">{product.description}</p>
+            <div className="flex flex-column flex-row-ns mb2-s items-center-ns">
+              <h4 className="db dib-ns mt0 f5 fw6 lh-copy mb0-ns">${product.price}</h4>
+              <a className="f6 link dim br3 ph5 pv2 ml3-ns dib-ns white bg-green tc" href="#0">Buy</a>
+            </div>
+          </div>
+        </section>
+        <section className="dt-row db">
+          <h1 className="f4 lh-title">Materials Included In This Kit:</h1>
+          <ul className="list pa0 measure">
+            {
+              product.materials.map(material => (
+                <li className="lh-copy pv3 ba bl-0 bt-0 br-0 b--dotted b--black-30">
+                  {material.title} - {material.quantity} {material.unit}
+                </li>
+              ))
+            }
+          </ul>
+        </section>
+        <section className="dt-row db">
+          <h1 className="f4 lh-title">Video</h1>
+          <YouTube
+            videoEmbedUrl={product.video_embed_url}
+          />
+        </section>
+      </div>
+    );
+  }
+}
