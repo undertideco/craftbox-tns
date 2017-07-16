9cb1f339a938fdbc26e3ac0cd7ccf8526b48cd15
Jurvis Tan <jurvis.tan@me.com>
2017-07-16 14:11:55 +0800

    make catalogitem’s photo square

--- src/components/CatalogItem/catalog_item.jsx
+++ src/components/CatalogItem/catalog_item.jsx
@@ -4,10 +4,16 @@ import Link from 'gatsby-link';
 class CatalogItem extends Component {
   render() {
     const { item } = this.props;
+    const imageStyle = {
+      backgroundImage: `url(${item.image})`,
+    };
+
     return (
       <article className="fl w-100 w-25-m w-20-l mv4 pa2-ns">
         <Link className="db link dim" to={`/product?id=${item.id}`}>
-          <img src={item.image} className="db w-100" alt="Photo of a kitten looking menacing." />
+	  <div className="aspect-ratio aspect-ratio--1x1">
+	    <img style={imageStyle} className="bg-center cover aspect-ratio--object" />
+	  </div>
           <dl className="mt2 f6 lh-copy">
             <dt className="clip">Title</dt>
             <dd className="f5 fw5 ml0 black truncate w-100">{item.title}</dd>
